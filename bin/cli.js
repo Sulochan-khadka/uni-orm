315bd6caef7cf647ca1ff478d28b7724f13aaa66
Sulochan Khadka <122200551+Sulochan-khadka@users.noreply.github.com>
2025-08-09 00:32:49 +0530

    feat: add standalone binary build

--- bin/cli.js
+++ bin/cli.js
@@ -2,16 +2,11 @@
 
 const { program } = require('commander');
 const chalk = require('chalk');
-const figlet = require('figlet');
-const { UniORM } = require('../src/core/UniORM');
-const { LanguageDetector } = require('../src/core/LanguageDetector');
-const { MigrationManager } = require('../src/core/MigrationManager');
-const { DashboardManager } = require('../src/dashboard/DashboardManager');
 
 // Display banner
 console.log(
   chalk.cyan(
-    figlet.textSync('UniORM', { horizontalLayout: 'full' })
+    `\n  _   _       _  ___  ____  __  __\n | | | |_ __ (_)/ _ \\|  _ \\|  \\/  |\n | | | | '_ \\| | | | | |_) | |\\/| |\n | |_| | | | | | |_| |  _ <| |  | |\n  \\___/|_| |_|_|\\___/|_| \\_\\_|  |_|\n`
   )
 );
 console.log(chalk.yellow('Universal ORM for all programming languages\n'));
@@ -27,6 +22,7 @@ program
   .option('-l, --language <language>', 'specify programming language')
   .option('-d, --database <database>', 'specify database type')
   .action(async (options) => {
+    const { UniORM } = require('../src/core/UniORM');
     const uniorm = new UniORM();
     await uniorm.initialize(options);
   });
@@ -35,6 +31,7 @@ program
   .command('detect')
   .description('Detect programming language and database in current project')
   .action(async () => {
+    const { LanguageDetector } = require('../src/core/LanguageDetector');
     const detector = new LanguageDetector();
     const result = await detector.detectAll();
     console.log(chalk.green('Detection results:'));
@@ -48,6 +45,7 @@ program
   .option('-t, --to <database>', 'target database')
   .option('--type <type>', 'migration type: schema, data, or both', 'both')
   .action(async (options) => {
+    const { MigrationManager } = require('../src/core/MigrationManager');
     const migrationManager = new MigrationManager();
     await migrationManager.migrate(options);
   });
@@ -57,6 +55,7 @@ program
   .description('Launch web dashboard for visual database management')
   .option('-p, --port <port>', 'port number', '3000')
   .action(async (options) => {
+    const { DashboardManager } = require('../src/dashboard/DashboardManager');
     const dashboardManager = new DashboardManager();
     await dashboardManager.start(options.port);
   });
@@ -66,6 +65,7 @@ program
   .description('Generate ORM models and configurations')
   .option('-t, --type <type>', 'generation type: models, config, or all', 'all')
   .action(async (options) => {
+    const { UniORM } = require('../src/core/UniORM');
     const uniorm = new UniORM();
     await uniorm.generate(options);
   });
@@ -75,6 +75,7 @@ program
   .description('Sync database schema with ORM models')
   .option('--force', 'force sync (destructive)')
   .action(async (options) => {
+    const { UniORM } = require('../src/core/UniORM');
     const uniorm = new UniORM();
     await uniorm.sync(options);
   });
